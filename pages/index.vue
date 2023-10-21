df165f1f7d80cee6051a2cd5fb98c219f8b5d2aa
Angelillo15 <contact@angelillo15.es>
2023-10-21 14:49:46 +0200

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <Welcome />
+  <FeatureFeatures />
+</template>
+
+<script lang="ts" setup>
+const { t } = useI18n();
+useHead({
+  title: t("index.meta.title"),
+  meta: [
+    {
+      name: "description",
+      content: t("index.meta.description"),
+    },
+  ],
+});
+</script>
---
+++ pages/products/index.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import products from '~/data/products';
+const { t } = useI18n();
+
+useHead({
+  title: t("products.meta.title"),
+  meta: [
+    {
+      name: "description",
+      content: t("products.meta.description"),
+    },
+  ],
+});
+</script>
+
+<template>
+  <header class="text-center pulse xl:text-7xl md:text-6xl text-4xl my-8">
+    <h1 v-html="$t('resource.header')" />
+  </header>
+
+  <section class="flex justify-center mt-24 flex-wrap gap-4 my-16">
+    <Product v-for="product in products" :key="product.name" :product="product" />
+  </section>
+</template>
